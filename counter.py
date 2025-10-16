c1e26c882e1a6c1728e1f60d0ca189e3f0838e39
lydsleepy <jiahua.mp3@gmail.com>
2025-10-15 23:25:50 -0500

    fixed MORE THINGS

--- counter.py
+++ counter.py
@@ -7,20 +7,21 @@ class Counter:
         data = Storage.load_data()
         self.total = data['total']
         self.current = data['current']
+        self.remaining = self.get_remaining()
 
     def increment(self):
     # increments the current count by 1
     # not limited to the total count
         self.current += 1
         self._save()
-        return True
+        return self.current
     
     def decrement(self):
     # decrements the current count by 1
         if self.current > 0:
             self.current -= 1
             self._save()
-            return True
+            return self.current
         return False
 
     def set_current(self, value):
@@ -30,7 +31,7 @@ class Counter:
             if value >= 0:
                 self.current = value
                 self._save()
-                return True
+                return self.current
         except (ValueError, TypeError):
             pass
         return False
@@ -42,7 +43,7 @@ class Counter:
             if value >= 0:
                 self.total = value
                 self._save()
-                return True
+                return self.total
         except (ValueError, TypeError):
             pass
         return False
@@ -57,14 +58,14 @@ class Counter:
 
     def get_remaining_text(self):
     # formatted remaining string
-        if remaining > 0:
-            return f"{abs(remaining)} OVER THE GOAL!!!"
-        elif remaining == 0:
+        if self.remaining < 0:
+            return f"{abs(self.remaining)} OVER THE GOAL!!!"
+        elif self.remaining == 0:
             return "LETS GOOO YOU DID IT!!!"
-        elif remaining == 1:
+        elif self.remaining == 1:
             return "YOU'RE LITERALLY SO CLOSE"
         else:
-            return f"{remaining} left, lock tf in"
+            return f"{self.remaining} left, lock tf in"
 
     def _save(self):
     # save state
